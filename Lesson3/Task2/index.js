e25cc941c09ae7cd6d9ae09aaed6c33601852b19
MarinaSavchenkoMarushchak <masi18@i.ua>
2022-06-18 22:15:42 +0300

    1

---
+++ Lesson3/Task2/index.js
@@ -0,0 +1,30 @@
+// code below must be finished ;)
+
+/* cast to string */
+console.log('17 to string is 17')
+console.log('-17.17 to string is -17.17')
+console.log('false to string is false')
+console.log('null to string is null')
+console.log('undefined to string is undefined')
+console.log('0 to string is 0')
+
+/* cast to number */
+console.log("'17' to number is 17")
+console.log('true to number is 1')
+console.log('false to number is 0')
+console.log('null to number is 0')
+console.log('undefined to number is NaN')
+console.log("'   20   ' to number is 20")
+console.log("'      ' to number is 0")
+console.log("'   30d   ' to number is NaN")
+
+/* cast to boolean */
+console.log('null to boolean is false')
+console.log('undefined to boolean is false')
+console.log('0 to boolean is false')
+console.log('-0 to boolean is false')
+console.log('NaN to boolean is false')
+console.log("'' to boolean is false")
+console.log("' ' to boolean is true")
+console.log('17 to boolean is true')
+console.log("'Hello' to boolean is true")
